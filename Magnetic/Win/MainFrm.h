e694f4b39d66b89f4031b1045800e4e3ae58d1ea
Joan CiberSheep <cibersheep@gmail.com>
2022-02-23 19:35:20 +0100

    Add Magnetic Scrolls interpreter from upstream

---
+++ Magnetic/Win/MainFrm.h
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+// Magnetic 2
+// Magnetic Scrolls Interpreter
+//
+// Visual C++ MFC Windows interface by David Kinder
+//
+// MainFrm.h: Declaration of the frame class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+#include "MenuBar.h"
+
+class CMainFrame : public MenuBarFrameWnd
+{
+protected: // create from serialization only
+  CMainFrame();
+  DECLARE_DYNCREATE(CMainFrame)
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+  // ClassWizard generated virtual function overrides
+  //{{AFX_VIRTUAL(CMainFrame)
+  public:
+  virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+  virtual BOOL DestroyWindow();
+  //}}AFX_VIRTUAL
+
+// Implementation
+public:
+  virtual ~CMainFrame();
+#ifdef _DEBUG
+  virtual void AssertValid() const;
+  virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:  // control bar embedded members
+  CStatusBar m_statusBar;
+
+// Generated message map functions
+  //{{AFX_MSG(CMainFrame)
+  afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+  afx_msg void OnPaletteChanged(CWnd* pFocusWnd);
+  afx_msg BOOL OnQueryNewPalette();
+  //}}AFX_MSG
+  afx_msg LRESULT OnDpiChanged(WPARAM, LPARAM);
+
+  DECLARE_MESSAGE_MAP()
+
+  int m_dpi;
+};
